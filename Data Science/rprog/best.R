776bb7f9376424cdc2af6eaeee3942b9e21c21fb
zhangjie <luoyetx@gmail.com>
2014-04-22 22:32:13 +0800

    week4 PA

---
+++ Data Science/rprog/best.R
@@ -0,0 +1,21 @@
+best <- function(state, outcome) {
+    ## Read outcome data
+    data <- read.csv("outcome-of-care-measures.csv", colClasses="character")
+    ## Check that state and outcome are valid
+    if (sum(as.numeric(data$State == state)) == 0)
+        stop("invalid state")
+    outcome.valid <- c("heart attack", "heart failure", "pneumonia")
+    if (sum(as.numeric(outcome.valid == outcome)) == 0)
+        stop("invalid outcome")
+    ## Return hospital name in that state with lowest 30-day death
+    ## rate
+    cols <- if (outcome == "heart attack") c(2, 11)
+    else if (outcome == "heart failure") c(2, 17)
+    else c(2, 23)
+    hospital <- data[data$State == state, cols]
+    hospital[, 2] <- as.numeric(hospital[, 2])
+    good <- is.na(hospital[, 2]) == F
+    hospital <- hospital[good,]
+    sel <- hospital[, 2] == min(hospital[, 2])
+    sort(hospital[sel, 1])[1]
+}
---
+++ Data Science/rprog/rankall.R
@@ -0,0 +1,31 @@
+rankall <- function(outcome, num = "best") {
+    ## Read outcome data
+    data <- read.csv("outcome-of-care-measures.csv", colClasses="character")
+    ## Check that state and outcome are valid
+    if (!(outcome %in% c("heart attack", "heart failure", "pneumonia")))
+        stop("invalid outcome")
+    ## For each state, find the hospital of the given rank
+    cols <- if (outcome == "heart attack") c(2, 7, 11)
+            else if (outcome == "heart failure") c(2, 7, 17)
+            else c(2, 7, 23)
+    hospital <- data[, cols]
+    hospital[,3] <- as.numeric(hospital[,3])
+    hospital <- hospital[!is.na(hospital[,3]), ]
+    hospital.sorted <- hospital[order(hospital[,2],hospital[,3],hospital[,1]),]
+    ## Return a data frame with the hospital names and the
+    ## (abbreviated) state name
+    if (num == "best") num <- 1
+    states <- unique(hospital.sorted$State)
+    result <- data.frame(hospital=c(NA), state=c(NA))
+    ## iteration for states
+    for (state in states) {
+        hospital.state <- hospital.sorted[hospital.sorted$State == state,]
+        if (num == "worst") num.state <- nrow(hospital.state)
+        else num.state <- num
+        if (num.state > nrow(hospital.state))
+            result[nrow(result)+1,] <- c(NA, state)
+        else
+            result[nrow(result)+1,] <- c(hospital.state[num.state,1],state)
+    }
+    result[2:nrow(result),]
+}
